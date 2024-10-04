cd02fea6128e8af5a262b8c88cd7ff414f562609
TerraDash <114143622+TerraDash@users.noreply.github.com>
2024-10-04 16:13:47 +0200

    slight change to camera (strict follow mode)

--- boom/Assets/CameraMovement.cs
+++ boom/Assets/CameraMovement.cs
@@ -12,6 +12,8 @@ public class CameraMovement : MonoBehaviour
     //used for smooth camera movement. lower number causes camera to lag behind player more
     public float velocityScalingFactor = 0.2f;
     //used for zooming out when the player is moving fast
+    public float StrictFollowTreshold;
+    public float StrictFollowMultiplier;
     float zPosInit;
     //the camera zoom position; locked in at runtime
     void Start()
@@ -23,10 +25,13 @@ public class CameraMovement : MonoBehaviour
     void FixedUpdate()
     {
         {
+            float dampMult = 1f;
+            Vector2 objectPos = Camera.main.WorldToScreenPoint(playerLoc.position);
+            if(objectPos.x>=StrictFollowTreshold*Screen.width || objectPos.x<=StrictFollowTreshold*Screen.width*-1 || objectPos.y>=StrictFollowTreshold*Screen.height || objectPos.y<=StrictFollowTreshold*Screen.height*-1 ){dampMult = StrictFollowMultiplier; Debug.Log("strict follow");}
             transform.position = Vector3.Lerp(transform.position, 
                                             new Vector3(playerLoc.position.x, playerLoc.position.y, 
-                                            zPosInit + velocityScalingFactor*Mathf.Sqrt(playerRB.velocity.x*playerRB.velocity.x + playerRB.velocity.y*playerRB.velocity.y)), 
-                                            Time.fixedDeltaTime * damping);
+                                            zPosInit - velocityScalingFactor*Mathf.Sqrt(playerRB.velocity.x*playerRB.velocity.x + playerRB.velocity.y*playerRB.velocity.y)), 
+                                            Time.fixedDeltaTime * damping * dampMult);
         }
 
     }
