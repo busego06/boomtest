0988abcac4dfc87037632af30b55506b6edd339d
TerraDash <114143622+TerraDash@users.noreply.github.com>
2024-10-10 17:32:17 +0200

    changed

--- boom/Assets/control.cs
+++ boom/Assets/control.cs
@@ -11,6 +11,9 @@ public class control : MonoBehaviour
     public Vector3 mousePosDelta;
     public Vector3 objectPos;
     public float boomForce;
+    public float recoilForce;
+    public float reloadTime;
+    float reloadCounter;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,11 +29,18 @@ public class control : MonoBehaviour
         //direction of the mouse relative to the player
         mousePosDelta = Vector3.Normalize(mousePos - objectPos);
         //when Fire1 is pressed. Fire1 defaults to Mouse1 (left mouse button) iirc
-        if(Input.GetButtonDown("Fire1")){
+        if(Input.GetButtonDown("Fire1")&&reloadCounter<=0){
             /*Debug.Log(mousePosDelta); debug code, shows mousePosDelta in the debugger*/
-            //creates an explosion object a small distance from the player, between the player and the mouse, with no rotation.
-            GameObject.Instantiate(explosion, mousePosDelta*explosionDistance + transform.position, new Quaternion(0,0,0,0));
+            //raycast
+            RaycastHit2D surfacePoint = Physics2D.Raycast(transform.position + mousePosDelta*explosionDistance, mousePosDelta); 
+            //creates an explosion on the nearest surface in the direction of the mouse as seen from the player.
+            if(surfacePoint.collider != null){
+            GameObject.Instantiate(explosion, new Vector3(surfacePoint.point.x, surfacePoint.point.y, transform.position.z), new Quaternion(0,0,0,0));}
+            //recoil
+            thisrb.AddForce(-1*mousePosDelta*recoilForce, ForceMode2D.Impulse);
+            reloadCounter = reloadTime;
         }
+        reloadCounter-=Time.deltaTime;
     }
     // is called whenever the player overlaps with a collider that has "IsTrigger" set to true
     void OnTriggerEnter2D(Collider2D Collider){
